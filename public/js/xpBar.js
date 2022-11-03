3cbc25fbad261fe02cf422f64f3a53c5b97cd258
alexandre <chavand.alexandre@laposte.net>
2022-11-03 19:07:39 +0100

    update

--- public/js/xpBar.js
+++ public/js/xpBar.js
@@ -1,15 +1,8 @@
-//let xpBar = document.querySelector(".xpBar");
-
 let progress = document.querySelector(".xpBarProgress");
 
-// Récupèrer les datasets de progress
 const current     = progress.dataset.currentxp;
 const total       = progress.dataset.totalxp;
 
-// A partir de ces dataset, créer un pourcentage
 let percentage    = Toolbox.createPercentage(current, total);
 
-// Modifier la propriété css width de progress pour lui attribuer le nouveau pourcentage
-console.log(percentage);
 progress.style.width = percentage + "%" ;
-console.log(progress.style.width)
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\DisconnectedPresentation;
 use App\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Entity\DisconnectedPresentation;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
+    #[IsGranted("ROLE_ADMIN")]
     public function index(): Response
     {
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
